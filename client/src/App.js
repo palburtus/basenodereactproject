ea1a2476d34abf3aa60e0ac43ba41916175e689c
Patrick Alburtus <palburtus@webmd.net>
2020-01-16 14:59:03 -0500

    modified app.js and package.json to run client and server via proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home.jsx'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  const UnAuthenticatedContainer = () => (
+    <div>
+      <div className="container body-content">
+        <Route path="/home" component={Home} />         
+      </div>
     </div>
+   )
+  
+   function NoMatch({ location }) {
+    return (
+      <div>
+        <h3>
+          404!
+        </h3>
+        <p>Could not find page <code>{location.pathname}</code></p>
+      </div>
+    );
+  }
+
+  return (
+    
+    <Router>
+        <Switch>
+          <div className="App">
+            <Route exact path="/home" component={UnAuthenticatedContainer}/> 
+          </div>
+        </Switch>
+      </Router>
   );
 }
 
